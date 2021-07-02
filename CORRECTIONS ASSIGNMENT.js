d46a068aa62b000f2643e34d3794f9c73760a327
Obaro Winner <85342925+winnerneo@users.noreply.github.com>
2021-07-02 11:59:50 +0100

    CORRECTED

---
+++ CORRECTIONS ASSIGNMENT.js
@@ -0,0 +1,49 @@
+const increaseBtn = document.querySelector('#increase');
+const resetBtn = document.querySelector('#reset');
+const decreaseBtn = document.querySelector('#decrease');
+
+
+let count = 0;
+document.getElementById('num').innerHTML = count;
+
+increaseBtn.addEventListener('click', function() {
+ count++;
+    document.getElementById('num').innerHTML = count;
+    if (count > 0)
+    {
+        document.getElementById('num').style.color='green'
+    }
+    else if (count === 0)
+    {
+        document.getElementById('num').style.color='black'
+    }
+});
+
+
+
+decreaseBtn.addEventListener('click', function() {
+    count--;
+       document.getElementById('num').innerHTML = count;
+       if (count < 0)
+       {
+           document.getElementById('num').style.color='red'
+       }
+       else if (count === 0)
+       {
+           document.getElementById('num').style.color='black'
+       }
+   });
+
+   resetBtn.addEventListener('click', function() {
+    count = 0;
+       document.getElementById('num').innerHTML = count;    
+       if(count === 0)
+       {
+           document.getElementById('num').style.color='black'
+       }
+       
+   })
+
+let con = 55;
+console.log('ijeoma is' + con)
+   
