cf809aa751c882bbd17d7e01ce06e1db4dd681d5
Obaro Winner <85342925+winnerneo@users.noreply.github.com>
2021-07-02 11:58:26 +0100

    CLASS WORK

---
+++ CLASS WORK/classwork.js
@@ -0,0 +1,24 @@
+// const num =64
+// const result= Math.sqrt(num);
+// console.log(`the square root of ${num} is ${result}`)
+
+
+// function Squareroot(){result}
+    
+
+
+// const number = prompt ('Please Enter The NUmber')
+// let sqaureroot = Math.sqrt(number);
+// console.log(sqaureroot)
+
+// document.getElementById('sqr').innerHTML =`The square Root of ${number} is ${sqaureroot}`
+
+
+
+const base =prompt('Enter the base of the triangle')
+const Height =prompt('Enter the height of the triangle')
+
+let Area = 1/2* base * Height
+
+console.log(`The Area is ${Area}`)
+document.getElementById('area').innerHTML =`The Area is ${Area}`
